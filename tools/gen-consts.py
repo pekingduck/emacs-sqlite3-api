77e719fb8850e55bda3068a713d99d8a4547379c
Y. N. Lo <gordonynlo@yahoo.com>
2017-09-05 11:14:05 +0800

    better comments

--- tools/gen-consts.py
+++ tools/gen-consts.py
@@ -6,10 +6,11 @@ import re
 
 for line in sys.stdin.readlines():
   line.rstrip()
+  # fields = [ "#define", "SQLITE_XXXX" "YYYY" ];
   fields = re.split("\s+", line, 3)
-  name = re.sub("_", "-", fields[1].lower())
+  sym = re.sub("_", "-", fields[1].lower())
   if len(fields) > 2 and fields[2] != "":
     if fields[2].startswith('"'):
-      print('defconst(env, "{0}", env->make_string(env, {1}, strlen({1})));'.format(name, fields[1]))
+      print('defconst(env, "{0}", env->make_string(env, {1}, strlen({1})));'.format(sym, fields[1]))
     else:
-      print('defconst(env, "{0}", env->make_integer(env, {1}));'.format(name, fields[1]))
+      print('defconst(env, "{0}", env->make_integer(env, {1}));'.format(sym, fields[1]))
